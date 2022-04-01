345ddac8a5fe422b8b9abf4d65cda4bec91424eb
senthilkumar <senthilkumar@iodinesoftware.com>
2022-04-01 15:54:06 +0530

    Search in Rotated Sorted Array while loop logic

--- SearchInRotatedSortedArray/index.js
+++ SearchInRotatedSortedArray/index.js
@@ -3,13 +3,14 @@
  * @param {number} target
  * @return {number}
  */
- var search = function(nums, target) {
+ var searchUsingWhileLoop = function(nums, target) {
     let firstIndex = 0;
     let lastIndex = nums.length - 1;
-     
+    console.time('searchUsingWhileLoop');
      while (firstIndex <= lastIndex) {
          let middle = Math.floor((firstIndex + lastIndex) / 2);
          if(target === nums[middle]) {
+            console.timeEnd('searchUsingWhileLoop');
              return middle;
          }
          //Left portion
@@ -30,10 +31,35 @@
              }
          }
      }
+     console.timeEnd('searchUsingWhileLoop');
+  return -1;
+};
+var searchUsingForLoop = function(nums, target) {
+    console.time('searchUsingForLoop')
+    let firstIndex = 0;
+    let lastIndex = nums.length - 1;
+     
+    for (let i = 0; i < nums.length; i++)   {
+        let middle = (lastIndex - firstIndex)/2;
+        if (nums[i] === target)  {
+            console.timeEnd('searchUsingForLoop');
+            return i;
+        }
+        else if (nums[middle] > target)  {
+            lastIndex = middle - 1;
+        }
+        else    {
+            firstIndex = middle + 1;
+        }
+    }
+    console.timeEnd('searchUsingForLoop');
   return -1;
 };
 
-console.log(search([4,5,6,7,0,1,2], 0));
-console.log(search([4,5,6,7,0,1,2], 3));
-console.log(search([1], 1));
+console.log(searchUsingWhileLoop([4,5,6,7,0,1,2], 0));
+console.log(searchUsingWhileLoop([4,5,6,7,0,1,2], 3));
+console.log(searchUsingWhileLoop([1], 1));
+console.log(searchUsingForLoop([4,5,6,7,0,1,2], 0));
+console.log(searchUsingForLoop([4,5,6,7,0,1,2], 3));
+console.log(searchUsingForLoop([1], 1));
 
